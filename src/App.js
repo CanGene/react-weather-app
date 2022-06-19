679fa812816ae672333d6f540dc0eed900d1a7ef
CanGene <86260135+CanGene@users.noreply.github.com>
2022-06-19 10:24:03 -0700

    Added footer credits.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by Candace Genesis and is{" "}
+        <a
+          href="https://github.com/CanGene/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub.
+      </footer>
+    </div>
+  );
 }
