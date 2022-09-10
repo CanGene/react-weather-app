3e55f93529d78583f4fe6998acffc2b8266e11c3
Candace Genesis <candace.genesis@gmail.com>
2022-09-09 21:54:28 -0700

    Added refresh to Weekly Forecast
    useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from "react";
-import "./WeatherForecast.css";
 import axios from "axios";
+import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
-    console.log(forecast);
   }
 
   if (loaded) {
-    console.log(forecast);
-
     return (
       <div className="WeatherForecast">
         <div className="row">
